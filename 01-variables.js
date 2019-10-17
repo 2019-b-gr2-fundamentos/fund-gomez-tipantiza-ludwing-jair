611a5ebadae26f7458a8a4314438e10d16a9bee6
Jair Gómez <56000432+Jairlgt15@users.noreply.github.com>
2019-10-17 17:19:09 -0500

    dbsdn.akLD

--- 01-variables.js
+++ 01-variables.js
@@ -1,5 +1,19 @@
+/*
+Nombre: Jair Gómez
+Fecha actualización:17-10-2019
+Desceipción: Ejemplos de variables primitas en JS
+*/
 \\ Comentario
-
 /* < - abro bloque comentario
 
 cierro bloque comentario -> */
+// Primitivas (Tipos de variable)
+/*
+int                      ---> numero ENTERO Ej:5 o -3 o 0
+char                     --->caracter       Ej:   a o b o c 
+float                    --->Numeros decimal  Ej:12.12 o -14.3
+double                   --->Numeros decimales++ Ej:1212..................
+string                   --->Arreglo de caracteres Ej:"abc" "cd"
+null                     --->VACIO Ejm:NULL
+boolen                   ----> VERDADERO O FALSO Ejmpl:tru o false 
+*/
