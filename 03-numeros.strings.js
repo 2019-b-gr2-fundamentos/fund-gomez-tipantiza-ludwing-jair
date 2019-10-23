cac90ce05e6d414fe0bfba417ea574bd0e10f8ba
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2019-10-23 17:56:56 -0500

    strings 1

---
+++ 03-numeros.strings.js
@@ -0,0 +1,27 @@
+var numeroUno = 1; //camelCase(todas minisculas)
+var numeroDos = 2; //camelCase
+var numeroSiete = 7;
+var numeroCuatro = 4;
+var resultado = 0;
+//SUMA
+resultado = 1 + 2;
+//RESTAR
+resultado = 1 - 2;
+//MULTIPLICACIÓN
+resultado = 1 * 2;
+//DIVIDIR
+resultado = 1 / 2;
+//EXPONENCIAL
+resultado = 1 ^ 2;
+// MOD
+resultado = 4 % 2; //o ---> residuo
+resultado = 4 % 3; //1 ---> residuo
+resultado = 7 % 3; //1 ---> residuo
+console.log("pera" + 2);
+// NUMEROUNO --->Todo mayuscula separación
+// NUMERO_UNO ---> mayusucula guion bajo separación
+// NumeroUno  ---> mayuscula cada palabra 
+// numerouno
+// numero_uno  -> PYTHON
+// numerOunO
+// CONSULTAR CUAL DE ESTOS TIPOS SE UTILIZA 
