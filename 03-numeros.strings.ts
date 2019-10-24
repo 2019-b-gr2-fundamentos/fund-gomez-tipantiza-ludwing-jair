88fbfeaf36ee5cd6c2d959e517aaeca15bbea8e5
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2019-10-24 18:19:25 -0500

    Strings

--- 03-numeros.strings.ts
+++ 03-numeros.strings.ts
@@ -20,6 +20,37 @@ resultado = 7 % 3; //1 ---> residuo
 
 console.log("pera" + 2);
 console.log("1 + 2");
+// STRINGS
+const mensaje = "Hola Mundo";
+const saludo = '"\"Adios Mundo"\"';
+console.log(mensaje + " " + saludo);
+const saludoCompleto = `${mensaje}\n${saludo}`;
+//Contamos los elementos
+// template strings
+const edad = 30;
+const saludoEdad= `Mi edad es: \n ${edad}`;
+console.log(saludoEdad);
+const nombreLongitud = "Ludwing Jair";
+console.log(nombreLongitud.length);
+// traer una expresion regular DEBER
+// Eliminaar espacios al principio y al final
+console.log(nombreLongitud.trim());
+// cambiar mayusculas
+console.log(nombreLongitud.toUpperCase());
+console.log(nombreLongitud.toLowerCase());
+console.log(nombreLongitud.replace("a","o"));
+console.log(nombreLongitud.replace("i","o"));
+console.log(nombreLongitud.replace("x","o"));
+console.log(nombreLongitud.search ("x"));
+console.log(nombreLongitud.search ("Jair"));
+console.log(nombreLongitud.slice(0,7));
+console.log(nombreLongitud.indexOf("x"));
+console.log(nombreLongitud.includes("z"));
+console.log(nombreLongitud.includes("x"));
+console.log(nombreLongitud.startsWith("Jair"));
+console.log(nombreLongitud.startsWith("Lud"));
+console.log(nombreLongitud.substring(0,6));
+
 
 
 
