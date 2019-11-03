0b20429889ddda3df8f4610974974d80438b68cd
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2019-11-03 17:49:11 -0500

    arreglos

--- 05-prompt.ts
+++
@@ -1,5 +0,0 @@
-const numeroUnoStrting = prompt(Ingresa numero 1);
-const numeroDOsString = prompt(Ingresa numero 2);
-const numeroUno = Number(numeroUnoStrting);
-const numeroDos = Number(numerDosStrting);
-console.log(numeroUno+numeroDos );
