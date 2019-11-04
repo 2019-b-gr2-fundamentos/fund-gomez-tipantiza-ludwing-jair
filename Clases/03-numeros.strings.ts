f56b932feb8cd69836ab6dde563e8e0f0264a806
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2019-11-03 23:41:28 -0500

    deber en c++

--- Clases/03-numeros.strings.ts
+++ Clases/03-numeros.strings.ts
@@ -1,4 +1,4 @@
-const numeroUno = 1; //camelCase(todas minisculas)
+const numerouno = 1; //camelCase(todas minisculas)
 const numeroDos = 2; //camelCase
 const numeroSiete = 7;
 const numeroCuatro = 4;
@@ -33,6 +33,10 @@ console.log(saludoEdad);
 const nombreLongitud = "Ludwing Jair";
 console.log(nombreLongitud.length);
 // traer una expresion regular DEBER
+/*Expresiones regulares mediante una cadena literal para encontrar un patrón 
+para ello se usa el constructor de objeto RegExp
+ Para crear un literal RegExp se utiliza esta expresión var re = /regular expression/ */
+var re =/abc/;
 
 // Eliminaar espacios al principio y al final
 console.log(nombreLongitud.trim());
---
+++ Deberes/C++/Deberes  C++/hola mundo;) .cpp
@@ -0,0 +1,12 @@
+#include<iostream> // imput ouput stream (entrada y salida de datos
+using namespace std; // para optimizar la escritura de caracteres
+// C++ C PLUS PLUS CPP
+
+int main (){
+	
+	cout <<"Hola Mundo :D"<<endl;  // ce out
+	// <<endl endline \n  "espacio"
+	cout<<"Hola mundo\n";
+	return 0;
+	
+}
