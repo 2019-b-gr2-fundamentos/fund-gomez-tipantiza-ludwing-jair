a73880febc3795906be0df91e4fb44d73242047f
Adriana Enríquez <adriana.enriquez@epn.edu.ec>
2019-11-06 17:49:56 -0500

    no entendi nada.com

---
+++ Clases/02-iterables/01-while.ts
@@ -0,0 +1,19 @@
+
+
+
+
+/*
+1 analisis
+2 algebra
+3 compiladores
+5 program
+7  calculo
+11 fisica
+13 ingles
+Si es impar ---> aumentsamos a analisis socioeconomic
+si es par ----> aumentamos algebra
+si es muñtiplo de 3 ----> aumentamos a compiladores 
+*/
+
+
+
