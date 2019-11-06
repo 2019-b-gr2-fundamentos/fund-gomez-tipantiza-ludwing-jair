8f584f4d56a1ed5006ed9813577bcf47df779169
Jairlgt15 <56000432+Jairlgt15@users.noreply.github.com>
2019-11-06 10:51:40 -0500

    Update deber calculadora.js

--- Deberes/Typescript/Operaciones básicas/deber calculadora.js
+++ Deberes/Typescript/Operaciones básicas/deber calculadora.js
@@ -1,24 +1,25 @@
-const numeroUnostring = prompt("Ingresa numero 1"); // STRING
-const numeroDosString = prompt("Ingresa numero 2");
-const numeroUno = Number(numeroUnostring);
-const numeroDos = Number(numeroDosString);
-console.log(numeroUno + numeroDos);
+const numeroUnoString = prompt("Ingresa numero 1"); 
+const numeroDosString = prompt("Ingresa numero 2"); 
+const numeroUno = Number (numeroUnoString)
+const numeroDos = Number (numeroDosString);
+const operacionString = prompt ("Elije la Operación siguiendo la siguiente notacion 1:suma, 2:resta, 3: Multiplicaion, 4:division")
+const operacion = Number (operacionString);
 
-const numeroUnostring = prompt("Ingresa numero 1"); // STRING
-const numeroDosString = prompt("Ingresa numero 2");
-const numeroUno = Number(numeroUnostring);
-const numeroDos = Number(numeroDosString);
-console.log(numeroUno - numeroDos);
-
-const numeroUnostring = prompt("Ingresa numero 1"); // STRING
-const numeroDosString = prompt("Ingresa numero 2");
-const numeroUno = Number(numeroUnostring);
-const numeroDos = Number(numeroDosString);
-console.log(numeroUno * numeroDos);
-
-const numeroUnostring = prompt("Ingresa numero 1"); // STRING
-const numeroDosString = prompt("Ingresa numero 2");
-const numeroUno = Number(numeroUnostring);
-const numeroDos = Number(numeroDosString);
-console.log(numeroUno / numeroDos);
-
+if (operacion == 1 ){
+    console.log (numeroUno + numeroDos);
+} else {
+    if (operacion == 2){
+        console.log (numeroUno - numeroDos);
+    } else {
+        if(operacion == 3){
+            console.log (numeroUno * numeroDos);;
+        }else{
+            if(operacion == 4){
+                console.log (numeroUno / numeroDos);
+            }else{
+                console.log("El numero de la operacion no ha sido definida");
+            }
+            
+        }
+    }
+}
