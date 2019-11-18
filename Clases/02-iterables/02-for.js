8ccedc13cfad2ebb0d29422e867b53ca48a8f93c
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2019-11-18 15:26:51 -0500

    arreglos

---
+++ Clases/02-iterables/02-for.js
@@ -0,0 +1,57 @@
+//for
+// 1) Declarar una variable 
+// 2) Condición(Expresiones)
+//Incrementar,Disminuir
+/*for(let numeroExDeMiCrush= 7;numeroExDeMiCrush == 0;   //False
+    numeroExDeMiCrush--){
+    console.log('ES CAN DA LO ',numeroExDeMiCrush);numeroExDeMiCrush--;
+}*/
+/*for(let numeroExDeMiCrush= 7;numeroExDeMiCrush > 0;
+    numeroExDeMiCrush--){
+    console.log('ES CAN DA LO ',numeroExDeMiCrush);
+}*/
+/*for(let numeroExDeMiCrush= 0;numeroExDeMiCrush <7 ;
+    numeroExDeMiCrush++){
+    console.log('ES CAN DA LO ',numeroExDeMiCrush);} */
+for (var numeroExDeMiCrush = 0; numeroExDeMiCrush <= 6; numeroExDeMiCrush++) {
+    console.log('ES CAN DA LO ', numeroExDeMiCrush);
+}
+//ARREGLOS
+// Representacion de un vector 
+// 1 elemento (variable const numero = 1)
+// Conjunto del mismo elementos([1,2,3,4,5,6,7])
+// conjunto de diferentes elementos ([1,"abc",true])
+var arregloNumeros = [1, 2, 3, 4, 5];
+//const arregloNumeros = [1,2,3,4,5,6] queremos agregar 
+//Función(Push) 
+// Si existe una funcion elinmutable puede cambiar
+//lET REASIGNAR
+arregloNumeros.push(6);
+console.log('arreglo Numeros', arregloNumeros);
+arregloNumeros.pop();
+console.log('arreglo de Numeros', arregloNumeros);
+//Acceder a cada elemento del arreglo por el indice
+var indiceElementoUno = 0;
+arregloNumeros[indiceElementoUno];
+arregloNumeros[0];
+var indiceElementoCinco = 4;
+arregloNumeros[indiceElementoCinco];
+arregloNumeros[4];
+//Acceder al elemento 5
+// INDIE
+//[1,2,3,4,5,]
+// 0 1 2 3 4 
+//indice
+console.log(arregloNumeros[4]);
+// Arreglo 
+// 1) Elementos
+// 2) Indices(0 index based) --> Posicion
+// 3) Logitud -># Elementos
+//console.log(arregloNumeros.length);
+// For
+// let tamaño = arregloNumeros.length
+// tamaño >0
+//--
+/*for(let tamaño=arregloNumeros.length; tamaño > 0 ;
+    tamaño --){
+    console.log('ES CAN ',tamaño);}*/ 
