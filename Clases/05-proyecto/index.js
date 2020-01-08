acc8a21e1aaa8c6a2a4a0511fc2bd6c3a708bf48
Jairlgt15 <ludwing.gomez@epn.edu.ec>
2020-01-08 15:58:52 -0500

    ASDFGHJK

---
+++ Clases/05-proyecto/index.js
@@ -0,0 +1,6 @@
+//const sumaLibreria = require('libraryfundamentos/lib/suma'); //Node MODULES js
+//importar todo el contenido con el nombre sumaLibreria del paquete 'libreria-2019b/lib/suma'
+var sumaLibreria = require('libraryfundamentos/lib/suma');
+sumaLibreria(1, 2);
+var respuesta = sumaLibreria(1, 2);
+console.log("La respuesta es:", respuesta);
